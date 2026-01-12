774e0046cd4890f6d27d223cab6de4a1dbdd71f7
Nischal Paliwal <156404171+NischalPaliwal@users.noreply.github.com>
2026-01-12 10:30:35 +0530

    code added

--- multilingual_ai_voice_assistant/template.py
+++ multilingual_ai_voice_assistant/template.py
@@ -14,19 +14,17 @@ list_of_files = [
     "research/trials.ipynb"
 ]
 
-
 for filepath in list_of_files:
     filepath = Path(filepath)
     filedir, filename = os.path.split(filepath)
 
-    if filedir !="":
+    if filedir != "":
         os.makedirs(filedir, exist_ok=True)
-        logging.info(f"Creating directory; {filedir} for the file: {filename}")
+        logging.info(f"Creating directory {filedir} for the file: {filename}")
 
     if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
         with open(filepath, "w") as f:
-            pass
             logging.info(f"Creating empty file: {filepath}")
 
     else:
-        logging.info(f"{filename} is already exists")
+        logging.info(f"{filename} is already exists!")
